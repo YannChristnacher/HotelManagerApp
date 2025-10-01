2338c99a708c98f0875a0b4e5265896be9ee5d8b
Yann Christnacher <yann.christnacher@gmail.com>
2025-10-01 17:40:02 +0200

    feat: add hotels & pictures table migration

---
+++ services/api/database/migrations/2025_10_01_152620_pictures_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pictures', function (Blueprint $table) {
+            $table->id();
+            $table->string('model_type', 255);
+            $table->bigInteger('model_id');
+            $table->string('filepath', 255);
+            $table->integer('filesize');
+            $table->integer('position');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pictures');
+    }
+};
